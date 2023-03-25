8ed3e9fcb161db49fef4641216e088c1698157e9
XiuhaoDing <xiuhaod2@illinois.edu>
2023-03-24 21:25:52 -0500

    0324 version3

--- bookrent/accounts/urls.py
+++ bookrent/accounts/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('register/',views.register),
+    path('login/',views.login),
+    path('logout/',views.logout),
+    path('selfinfo/',views.selfinfo),
 ]
--- bookrent/accounts/views.py
+++ bookrent/accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
+from django.apps import apps
 # Create your views here.
 def register(request):
     if request.method == 'POST':
@@ -13,14 +14,45 @@ def register(request):
         if password1 == password2:
             if User.objects.filter(username = username).exists():
                 messages.info(request,"username already registered" )
+                return redirect("/accounts/register/")
             elif User.objects.filter(email = email).exists():
                 messages.info(request,"email already registered")
+                return redirect("/accounts/register/")
             else:
                 user = User.objects.create_user(username = username, email = email,password = password1,first_name = first_name, last_name = last_name)
                 user.save()
                 print("user created")
+                return redirect("/accounts/login")
         else:
-            print("password not match")
-        return redirect("/books/list")
+            messages.info(request,"password not match")
+            return redirect("/accounts/register/")
     else:
         return render(request,"register.html")
+    
+def login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = auth.authenticate(username = username,password = password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect("/accounts/selfinfo")
+        else:
+            messages.info(request,"incorrect password or username not exist")
+            return redirect("/accounts/login")
+    else:
+        return render(request,"login.html")
+
+def logout(request):
+    auth.logout(request)
+    return redirect("/books/list")
+
+def selfinfo(request):
+    if request.user.is_authenticated:
+        students = apps.get_model('books', 'students')
+        bks = students.objects.filter(netid = request.user.username)
+        return render(request, 'selfinfo.html',{'bks':bks})
+    else:
+        return render(request,"selfinfo.html")
