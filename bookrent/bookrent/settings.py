b6ede5390e5e637499318c93f1249efd9e82a7ef
chocolateman001102 <chocolateman001102@gmail.com>
2023-05-03 23:00:04 -0500

    list page table format

--- bookrent/bookrent/settings.py
+++ bookrent/bookrent/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': "bookrent",
         'USER':'postgres',
-        'PASSWORD':'12345678',
+        'PASSWORD':'001102',
         'HOST':'localhost',
         'PORT': '5432',
     }
